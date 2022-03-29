9a440009f3547f45e27f11ef79baec78135bf58f
Marian Bárta <Barta.Marian@stud.vassboskovice.cz>
2022-03-29 09:20:38 +0200

    tak nejak

--- OOP_Chess/Figure.cs
+++ OOP_Chess/Figure.cs
@@ -6,6 +6,23 @@ namespace OOP_Chess
 {
     public class Figure
     {
+        public FigureType Type { get; set; }
+        public string Position { get; set; } = "";
 
+        public Figure()
+        {
+            
+        }
+    }
+
+    // výčtový datový typ - seznam hodnot - interně sou to hodnoty integer (od 0)
+    public enum FigureType
+    {
+        Pawn,       // pěšec
+        Rook,       // věž 
+        Knight,     // jezdec 
+        Bishop,     // střelec 
+        Queen,      // dáma 
+        King,       // král
     }
 }
